eb7b0b844937265782519004af6c2bd28bab19ed
rdtvaacar <acarbey15@gmail.com>
2025-02-16 20:45:03 +0300

    Create AppDemoReactjs.js
    AppDemoReactjs

---
+++ AppDemoReactjs.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ApiSupport from "./components/ApiSupport";
+
+function App() {
+  return (
+    <div className="App">
+      <ApiSupport />
+    </div>
+  );
+}
+
+export default App;
