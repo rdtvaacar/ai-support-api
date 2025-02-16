da0b852bd9e3173eb2c31fee48075a297eb07070
rdtvaacar <acarbey15@gmail.com>
2025-02-16 20:43:31 +0300

    Create AppDemo.vue
    AppDemo

---
+++ AppDemo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <ApiSupport />
+  </div>
+</template>
+
+<script>
+import ApiSupport from "./components/ApiSupport.vue";
+
+export default {
+  components: {
+    ApiSupport,
+  },
+};
+</script>
